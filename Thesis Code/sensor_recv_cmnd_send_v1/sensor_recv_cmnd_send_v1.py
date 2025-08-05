10f0825302716c74c7c0e2bcf5907dd15504c01d
Tuck Forbes <tuckforbes@gmail.com>
2025-08-05 13:11:32 -0700

    Created first control law
    Updated sensor receive command send to have a very basic control law

--- Thesis Code/sensor_recv_cmnd_send_v1/sensor_recv_cmnd_send_v1.py
+++ Thesis Code/sensor_recv_cmnd_send_v1/sensor_recv_cmnd_send_v1.py
@@ -14,6 +14,7 @@ import numpy as np				# Library for matrices and vectors
 import sys						# Library for accessing I2C
 import os						# Library for navigating operating system
 import time						# Library for importing time
+import struct					# Library for converting from bytes 
 
 # Setting BMI160 Port address to the low port at 0x68 
 bmi160 = DFRobot_BMI160_IIC(addr = BMI160_IIC_ADDR_SDO_L) 
@@ -36,23 +37,72 @@ print("BMI160 connected.")
 # Main section where code runs till ended
 while True:
 # BMI160 section
-	data = bmi160.get_sensor_data() 		# Gather BMI160 data
+	imu_data = bmi160.get_sensor_data() 		# Gather BMI160 data
+	gyro_data = imu_data['gyro']
+	acc_data = imu_data['accel']
+	'''
 	# Display BMI160 Data
 	# Convert gyro from degrees to radians
-	print("gyro  :  x: %.3f rad/s,  y: %.3f rad/s,  z: %.3f rad/s"%(data['gyro']['x']*3.14/180.0, data['gyro']['y']*3.14/180.0, data['gyro']['z']*3.14/180.0))
+	print("gyro  :  x: %.3f rad/s,  y: %.3f rad/s,  z: %.3f rad/s"%(imu_data['gyro']['x']*3.14/180.0, imu_data['gyro']['y']*3.14/180.0, imu_data['gyro']['z']*3.14/180.0))
 	# Convert accelerometer data to gravity
-	print("accel :  x: %.3f g    ,  y: %.3f g    ,  z: %.3f g    "%(data['accel']['x']/16384.0, data['accel']['y']/16384.0, data['accel']['z']/16384.0))
+	print("accel :  x: %.3f g,  y: %.3f g,  z: %.3f g"%(imu_data['accel']['x']/16384.0, imu_data['accel']['y']/16384.0, imu_data['accel']['z']/16384.0))
 	print("\n")
+	'''
 	
 # Camera section
 	received_data = ser.read()              # Read serial port
 	time.sleep(0.05)						# Wait 0.05 seconds
 	data_left = ser.inWaiting()             # Check for remaining data
 	received_data += ser.read(data_left) 	# Combine data
+	'''
 	print (received_data)                   # Print received data
+	print (type(received_data))
+	'''
+	cam_data_decode = received_data.decode()
+	cam_data_list = cam_data_decode.split()	# Make data into list
+	'''
+	print(cam_data_list)	
+	print(type(cam_data_list))
+	'''
+	cam_data = np.zeros(6)
+	for i in range(6):
+		cam_data[i] = float(cam_data_list[i]) # make data into float
+	print(cam_data)
+	trans_data = cam_data[:3]
+	print(trans_data)
+	rot_data = cam_data[3:]
+	print(rot_data)
 	
+# Control law section
+	forw_dist = -1*trans_data[2]; 	# Data comes out - so mult by -1
+	l_r_dist = trans_data[1];	# Left is - right is +
+	print(l_r_dist)
+	rot_angle = -1*(rot_data[0]-180); # + is CW - is CCW (data is 0-360)
+	print(rot_angle)
+	'''
+	Step 1: Align with target in l/r direction
+	Step 2: Align with target in angle
+	Step 3: Drive to target
+	'''
+	l_r_tol = 1		# Set left right tolerance here
+	forw_tol = 0.5		# Set forward tolerance here
+	ang_tol = 5			# Set tolerance for angle here
+	
+	if abs(l_r_dist) >= l_r_tol:
+		if l_r_dist > 0: # If left of target move right
+			command = "Right"	# Set command to right
+		else: 	# If not left of target move left
+			command = "Left"	# Set command to left
+	elif abs(rot_angle) >= ang_tol:
+		if rot_angle > 0:
+			command = "CCW"
+		else: 
+			command = "CW"
+	else:
+		command = "Forward"
+		
 # Command send section
-	message = "Left" 						# Write Command
+	message = command 						# Write Command
 	sock.sendto(message.encode(), (UDP_IP, UDP_Port)) # Send Command
 	print(f"Sent: {message} to {UDP_IP}:{UDP_Port}") # Confirm send
 	time.sleep(0.1)  							# Wait 0.1 seconds
