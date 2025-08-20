86e9c78f2cb1eaf9cd82eb7f8fb6a859ce91c162
Tuck Forbes <tuckforbes@gmail.com>
2025-08-20 11:24:18 -0700

    Fixed error with while loops in movement code

--- Thesis Code/movement_v3.py
+++ Thesis Code/movement_v3.py
@@ -173,58 +173,62 @@ def lower_right(speed):
     rl_back()
     changespeed(speed)
 
+command = "Stop"
 
 while True:
-    data,addr = sock.recvfrom(1024) # data size and receive data
-	command = data.decode() # data type and unpack data here
+    try :
+        data,addr = sock.recvfrom(1024) # receive data
+    except :
+        command = command
+    else :
+        command = data.decode() # unpack data
+    finally: 
+        if command == "Forward":
+            print("Forward") 
+            go_ahead(move_speed)
+            
+        elif command == "Stop":
+            print("Stop")
+            stop_car()
 
-    while command == "Forward":
-        print("Forward") 
-        go_ahead(move_speed)
-        
-    while command == "Stop":
-        print("Stop")
-        stop_car()
-
-    while command == "Back":
-        print("Back")
-        go_back(move_speed)
-
-
-    while command == "Right":
-        print("Right")
-        shift_right(move_speed)
-
-
-    while command == "Left":
-        print("Left") 
-        shift_left(move_speed)
-
-    while command == "CW":
-        print("CW")
-        turn_right(move_speed)
-
-    while command == "CCW":
-        print("CCW")
-        turn_left(move_speed)
+        elif command == "Back":
+            print("Back")
+            go_back(move_speed)
 
 
-    while command == "UpR":
-        print("UpR")
-        upper_right(move_speed)
+        elif command == "Right":
+            print("Right")
+            shift_right(move_speed)
 
 
-    while command == "UpL":
-        print("UpL")
-        upper_left(move_speed)
+        elif command == "Left":
+            print("Left") 
+            shift_left(move_speed)
+
+        elif command == "CW":
+            print("CW")
+            turn_right(move_speed)
+
+        elif command == "CCW":
+            print("CCW")
+            turn_left(move_speed)
 
 
-    while command == "LowR":
-        print("LowR")
-        lower_right(move_speed)
+        elif command == "UpR":
+            print("UpR")
+            upper_right(move_speed)
 
 
-    while command == "LowL":
-        print("LowL")
-        lower_left(move_speed)
+        elif command == "UpL":
+            print("UpL")
+            upper_left(move_speed)
 
+
+        elif command == "LowR":
+            print("LowR")
+            lower_right(move_speed)
+
+
+        else command == "LowL":
+            print("LowL")
+            lower_left(move_speed)
